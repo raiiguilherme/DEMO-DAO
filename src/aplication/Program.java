361cb723d0cc1e0edcbb2b8fd529938ebbfaa482
raiiguilherme <raiguilherme322@gmail.com>
2024-01-18 08:13:46 -0300

    Adição do metodo FindAll

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,5 +1,7 @@
 package aplication;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.UsuarioDao;
 import model.entities.Usuario;
@@ -13,8 +15,18 @@ public class Program {
 		UsuarioDao usuariodao = DaoFactory.createUsuario();
 		
 		//instanciando um usuario que recebe um metodo puxado da instancia feita na parte de cima 
+		System.out.println("BUSCANDO POR ID");
+		System.out.println();
 		Usuario usuario = usuariodao.findById(23);
 		System.out.println(usuario);			//imprimindo
+		System.out.println();
+		
+		System.out.println("MOSTRANDO TODOS DA TABELA");
+		System.out.println();
+		List<Usuario> lis = usuariodao.findAll();
+		for (Usuario usuario2 : lis) {
+			System.out.println(usuario2);
+		}
 		
 		
 		
--- src/model/dao/impl/UsuarioDaoJDBC.java
+++ src/model/dao/impl/UsuarioDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -87,8 +88,43 @@ public class UsuarioDaoJDBC implements UsuarioDao {
 
 	@Override
 	public List<Usuario> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		//instanciando os objetos responsaveis pela manipulação no banco de dados
+				PreparedStatement ps = null;
+				ResultSet rs = null; 
+				
+				try {
+					
+					ps = conn.prepareStatement("SELECT * FROM usuario"); //query								
+					rs = ps.executeQuery();										//executa a query
+					
+					
+					List<Usuario> listu = new ArrayList<>();
+					//enquanto existir algum retorno na query:
+					while(rs.next()) {
+						//instancia de novo objeto chamado "usuario"
+					Usuario usuario = new Usuario();
+					//atribuindo o retorno do metodo rs.get() aos atributos do objeto "usuario"
+					usuario.setId(rs.getInt("id_usuario"));
+					usuario.setName(rs.getString("nome"));
+					usuario.setIdade(rs.getInt("idade"));
+					usuario.setEmail(rs.getString("email"));
+					usuario.setSenha(rs.getString("senha"));
+					
+					//salva na lista
+					listu.add(usuario);
+					
+					}
+						
+						return listu;
+					
+				}
+				catch (SQLException e) {
+					throw new DBException(e.getMessage());
+				}
+				finally {
+					DB.closeStatement(ps);
+					DB.closeResultSet(rs);
+				}
 	}
 
 }
